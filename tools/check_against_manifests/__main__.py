6dfb137c7defb2818e918b547d5bf48f7654bdb7
Isaac To <isaac.chun.to@gmail.com>
2024-11-20 12:45:57 -0800

    feat: provide main function
    Currently, just to run Pydantic validation
    across all dandiset metadata

---
+++ tools/check_against_manifests/__main__.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+from pydantic import TypeAdapter
+
+from dandischema.models import Dandiset, PublishedDandiset
+
+from .models import ValidationReport
+from .tools import iter_direct_subdirs, pydantic_validate
+
+MANIFEST_DIR = Path("/Users/isaac/Downloads/mnt/backup/dandi/dandiset-manifests-s3cmd")
+DANDI_SET_FILE_NAME = "dandiset.jsonld"
+
+validation_report_list_adapter = TypeAdapter(list[ValidationReport])
+
+
+def main():
+
+    validation_reports: list[ValidationReport] = []
+    for n, dandiset_dir in enumerate(
+        sorted(iter_direct_subdirs(MANIFEST_DIR), key=lambda p: p.name)
+    ):
+        # === In a dandiset directory ===
+        dandiset_identifier = dandiset_dir.name
+        print(f"{n}:{dandiset_identifier}: {dandiset_dir}")
+
+        for version_dir in iter_direct_subdirs(dandiset_dir):
+            # === In a dandiset version directory ===
+            dandiset_version = version_dir.name
+            print(f"\tdandiset_version: {dandiset_version}")
+
+            # Get the Pydantic model to validate against
+            if dandiset_version == "draft":
+                model = Dandiset
+            else:
+                model = PublishedDandiset
+
+            dandiset_metadata_file_path = version_dir / DANDI_SET_FILE_NAME
+
+            if dandiset_metadata_file_path.is_file():
+                dandiset_metadata = dandiset_metadata_file_path.read_text()
+                pydantic_validation_errs = pydantic_validate(dandiset_metadata, model)
+                validation_reports.append(
+                    ValidationReport(
+                        dandiset_identifier=dandiset_identifier,
+                        dandiset_version=dandiset_version,
+                        pydantic_validation_errs=pydantic_validation_errs,
+                    )
+                )
+
+    output_path = Path("validation_reports.json")
+    output_path.write_bytes(
+        validation_report_list_adapter.dump_json(validation_reports)
+    )
+
+
+if __name__ == "__main__":
+    main()
